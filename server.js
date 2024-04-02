3c87e978e822bba73c3f2455ca78680a0857d828
Mehmet Kaplan <mehmet.kaplan@gmail.com>
2024-04-03 00:49:57 +0300

    Fix incorrect parameter in expressServer function

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const {httpsKeys, port} = require('./server-parameters.js');
 const startServer = async () => {
 	// httpsKeys, p_port, handlerFile, functionsWhitelist, testFunctionsWhitelist
 	let whitelist =['webLogger'];
-	tes.expressServer(undefined, port, '/home/ubuntu/web-logger/handlers.js', whitelist, undefined);
+	tes.expressServer(httpsKeys, port, '/home/ubuntu/web-logger/handlers.js', whitelist, undefined);
 }
 
 startServer();
